898cfea993ec3a0206701bf59e7b43fd84d4e63d
JiaMming <l>
2019-08-06 13:56:44 +0800

    second

---
+++ src/views/movie/detail.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="detail">
+        <h1>datail</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'detail',
+        data() {
+            return{}
+        },
+        methods:{},
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
